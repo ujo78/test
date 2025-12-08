87fe761a872837091cc09944d87d3f9edb1093f7
RishavKumar12345678 <rishavpodar7@gmail.com>
2025-12-09 00:23:01 +0530

    Resheve

--- employees.py
+++ employees.py
@@ -1,3 +1,4 @@
+from  file_handling import *
 class employee():
     def __init__(self, cursor):
         self.cursor = cursor
@@ -8,16 +9,16 @@ class employee():
         self.name = None
         self.salary = None
         self.age = None
-    
+    @createTable_file_update
     def createTable(self):
         self.cursor.execute(f"CREATE TABLE IF NOT EXISTS {self.table_name}({self.colums[0]} INT , {self.colums[1]} VARCHAR(255) , {self.colums[2]} INT , {self.colums[3]} INT );")
-
+    @add_employee_file_update
     def add_employee(self):
         self.cursor.execute(f"INSERT INTO {self.table_name} VALUES ({self.id}  , '{self.name}' , {self.salary} , {self.age});")
-
+    @del_employee_file_update
     def del_employee(self):
         self.cursor.execute(f"DELETE FROM {self.table_name} WHERE {self.colums[0]} = {self.id};")
-
+    @update_employee_file_update
     def update_employee(self):
         self.cursor.execute(
             f"UPDATE {self.table_name} SET "
@@ -29,14 +30,14 @@ class employee():
 
 
        
-    
+    @search_employee_file_update
     def search_employee(self , searchbywhat , filterbywhat , searchinput , start , end):
      
         self.cursor.execute(f"select * from table_name where {searchbywhat} like {searchinput}% and  {filterbywhat} between {start} and {end}")
 
         return self.cursor.fetchall()
 
-
+    @sort_file_update
     def sort(self , sortbywhat , ascordsc = True):
         if ascordsc == True:
             self.cursor.execute(f"Select * from {self.table_name} order by {sortbywhat} ")
@@ -44,41 +45,8 @@ class employee():
             ascordsc = "DESC"
             self.cursor.execute(f"Select * from {self.table_name} order by {sortbywhat} {ascordsc} ")
         return self.cursor.fetchall()
-    
-
-<<<<<<< HEAD
-
-
-=======
+    @clear_table_file_update
     def clear_table(self):
         self.cursor.execute(f"DELETE FROM {self.table_name};")
         return self.cursor.fetchall()
-<<<<<<< HEAD
-    
 
-    
-
-    
-    
-
-    
-
-
-
-
-
-
-
-
-
-
-    
-
-    
-    
-
-
-
-=======
->>>>>>> a8d60bea281a3674589656f89515b2939b2710f8
->>>>>>> f13c194eb021e5e372d588a42dd2bc845f1fb485
